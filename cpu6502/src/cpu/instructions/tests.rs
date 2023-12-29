3fa0368eba1735fa9417e1c752bf421991f844fc
sarpt <mike.m.info@gmail.com>
2023-12-29 23:05:26 +0100

    Add cpy and cpx tests

--- cpu6502/src/cpu/instructions/tests.rs
+++ cpu6502/src/cpu/instructions/tests.rs
@@ -1899,3 +1899,183 @@ mod cmp {
         }
     }
 }
+
+#[cfg(test)]
+mod cpy {
+    #[cfg(test)]
+    mod cpy_im {
+        use crate::cpu::{instructions::cpy_im, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_y_register_with_next_byte_from_memory() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpy_im(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_one_cycle() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpy_im(&mut cpu);
+
+            assert_eq!(cpu.cycle, 1);
+        }
+    }
+
+    #[cfg(test)]
+    mod cpy_zp {
+        use crate::cpu::{instructions::cpy_zp, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_y_register_with_a_value_from_a_zero_page_address() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF, 0x00, 0x04])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpy_zp(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_two_cycles() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF, 0x00, 0x04])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpy_zp(&mut cpu);
+
+            assert_eq!(cpu.cycle, 2);
+        }
+    }
+
+    #[cfg(test)]
+    mod cpy_a {
+        use crate::cpu::{instructions::cpy_a, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_y_register_with_a_value_from_an_address() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0x00, 0x00, 0x03])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpy_a(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_three_cycles() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0x00, 0x00, 0x03])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpy_a(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+    }
+}
+
+#[cfg(test)]
+mod cpx {
+    #[cfg(test)]
+    mod cpx_im {
+        use crate::cpu::{instructions::cpx_im, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_x_register_with_next_byte_from_memory() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF])));
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpx_im(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_one_cycle() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF])));
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpx_im(&mut cpu);
+
+            assert_eq!(cpu.cycle, 1);
+        }
+    }
+
+    #[cfg(test)]
+    mod cpx_zp {
+        use crate::cpu::{instructions::cpx_zp, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_x_register_with_a_value_from_a_zero_page_address() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF, 0x00, 0x04])));
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpx_zp(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_two_cycles() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0xFF, 0x00, 0x04])));
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpx_zp(&mut cpu);
+
+            assert_eq!(cpu.cycle, 2);
+        }
+    }
+
+    #[cfg(test)]
+    mod cpy_a {
+        use crate::cpu::{instructions::cpx_a, tests::MemoryMock, CPU};
+
+        #[test]
+        fn should_compare_x_register_with_a_value_from_an_address() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0x00, 0x00, 0x03])));
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            assert_eq!(cpu.processor_status.flags, 0b00000000);
+
+            cpx_a(&mut cpu);
+
+            assert_eq!(cpu.processor_status.flags, 0b10000000);
+        }
+
+        #[test]
+        fn should_take_three_cycles() {
+            let mut cpu = CPU::new(Box::new(MemoryMock::new(&[0x03, 0x00, 0x00, 0x03])));
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            cpx_a(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+    }
+}
